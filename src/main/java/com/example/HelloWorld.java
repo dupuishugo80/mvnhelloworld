476c5635071aaed2aadb2379837e28bb562174b8
W137025 <hugo.dupuis@worldline.com>
2024-02-06 14:56:13 +0100

    TEST COMMIT 23dddd

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Commit 2");
+        System.out.println("Commit 2 dddd");
     }
 }
